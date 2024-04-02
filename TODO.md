04d22bf815c5dd331d27eb37a2de10c536c49c98
aliouche mohamed <m_aliouche@estin.dz>
2024-04-02 23:39:30 +0200

    resolving challenge feature added
    checking if the user didn't already solve the challenge than updating his points if he didn't

---
+++ TODO.md
@@ -0,0 +1,13 @@
+# Root-iyi- List of todos
+
+## new features
+- [ ] Ajouter une fonctionnalité de recherche avancée
+- [ ] Implémenter un système de notification par e-mail
+
+## bug fixes
+- [ ] fix the bug related to displaying all challenges
+
+## Enhancements
+- [ ] updating the db when a user solves a challenge is vulnerable to race condition, use transactions
+
+
--- models/challengeModele.php
+++ models/challengeModele.php
@@ -1,5 +1,6 @@
 <?php
     include("../connect.php");
+    $pdo = new PDO("mysql:host=mysql02.univ-lyon2.fr;dbname=php_moaliouche;charset=utf8mb4", "php_moaliouche", "xpf-FeenJiA7mEP4TE1KbuO4n");
     class Challenge{
         private $categoryId;
         private $title;
@@ -18,7 +19,7 @@
             $this->difficulty = $difficulty;
         }
         public function addChallenge(){
-            $pdo = new PDO("mysql:host=mysql02.univ-lyon2.fr;dbname=php_moaliouche;charset=utf8mb4", "php_moaliouche", "xpf-FeenJiA7mEP4TE1KbuO4n");
+            global $pdo;
             $res=$pdo->prepare("INSERT INTO `challenge` (`categoryId`,`title`,`description`,`urlChallenge`,`flag`,`nbPoints`,`difficulty`) VALUES (:categoryId,:title,:description,:urlChallenge,:flag,:nbPoints,:difficulty);");
             $res->bindParam(":title",$this->title);
             $res->bindParam(":description",$this->description);
@@ -30,12 +31,33 @@
             $res->execute();
         }
         public static function retreiveChallenge($id){
-            $pdo = new PDO("mysql:host=mysql02.univ-lyon2.fr;dbname=php_moaliouche;charset=utf8mb4", "php_moaliouche", "xpf-FeenJiA7mEP4TE1KbuO4n");
+            global $pdo;
             $res=$pdo->prepare("SELECT * FROM `challenge` WHERE `challengeId`=:challengeId;");
             $res->bindParam(":challengeId",$id);
             $res->execute();
             $row = $res->fetch(PDO::FETCH_ASSOC);
             return $row;
+        }
+        public static function validate($challengeId , $userId){
+            global $pdo;
+            $res = $pdo->prepare("SELECT * FROM `resolution` WHERE userId=:userId AND challengeId=:challengeId;");
+            $res->bindParam(":challengeId",$challengeId);
+            $res->bindParam(":userId",$userId);
+            $res->execute();
+            $row = $res->fetch(PDO::FETCH_ASSOC);
+            if($row["resolved"]){
+                return false;
+            }
+            else{
+                // this can be vulnerable to race condition
+                $res = $pdo->prepare("
+                UPDATE `user` SET nbPoints = nbPoints + (SELECT nbPoints FROM `challenge` WHERE challengeId=:challengeId) WHERE userId=:userId;
+                UPDATE `resolution` SET resolved = 1 Where userId=:userId AND challengeId=:challengeId;
+                ");
+                $res->bindParam(":challengeId",$challengeId);
+                $res->bindParam(":userId",$userId);
+                $res->execute();
+            }
         }
     }
 ?>
